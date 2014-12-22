7ea1ade0a2dfbb9277972e85dd9cab7f8d884d6b
dhalsim <unknown-user@192.168.0.13>
2014-12-22 22:16:41 +0200

    fixed directory for osx

--- Tests/UnitTests.RuleExperiments/AOP/ModuleWeaversFixture.cs
+++ Tests/UnitTests.RuleExperiments/AOP/ModuleWeaversFixture.cs
@@ -7,53 +7,54 @@ using Weavers;
 
 namespace UnitTests.RuleExperiments.AOP
 {
-    [TestFixture]
-    public class ModuleWeaversFixture
-    {
-        private ModuleWeaver _moduleWeaver;
+	[TestFixture]
+	public class ModuleWeaversFixture
+	{
+		private ModuleWeaver _moduleWeaver;
 
-        [TestFixtureSetUp]
-        public void Setup()
-        {
-            _moduleWeaver = new ModuleWeaver();
-            DefaultAssemblyResolver assemblyResolver = new DefaultAssemblyResolver();
+		[TestFixtureSetUp]
+		public void Setup()
+		{
+			_moduleWeaver = new ModuleWeaver();
+			DefaultAssemblyResolver assemblyResolver = new DefaultAssemblyResolver();
 
-            const string directory = @"C:\Users\BAydek\Documents\Visual Studio 2013\Projects\RuleExperimentsV2\Implementations\Application.RuleExperiments\bin\Debug";
-            assemblyResolver.AddSearchDirectory(directory);
+			string upDirectory = Path.Combine("..", "..", "..", "..");
+			string directory = Path.Combine(Directory.GetCurrentDirectory(), upDirectory, "Implementations", 
+				                   "Application.RuleExperiments", "bin", "Debug");
+			assemblyResolver.AddSearchDirectory(directory);
 
-            _moduleWeaver.ModuleDefinition = ModuleDefinition.ReadModule(Path.Combine(directory, "Application.RuleExperiments.dll"), new ReaderParameters
-            {
-                AssemblyResolver = assemblyResolver
-            });
+			_moduleWeaver.ModuleDefinition = ModuleDefinition.ReadModule(Path.Combine(directory, "Application.RuleExperiments.dll"), new ReaderParameters {
+				AssemblyResolver = assemblyResolver
+			});
 
-            _moduleWeaver.Execute();
-        }
+			_moduleWeaver.Execute();
+		}
 
-        [Test]
-        public void Should_find_class_TestClass()
-        {
-            Assert.IsNotNull(_moduleWeaver);
+		[Test]
+		public void Should_find_class_TestClass()
+		{
+			Assert.IsNotNull(_moduleWeaver);
 
-            var testClass = _moduleWeaver.ModuleDefinition.Types.FirstOrDefault(t => t.Name == "TestClass");
-            Assert.IsNotNull(testClass);
-        }
+			var testClass = _moduleWeaver.ModuleDefinition.Types.FirstOrDefault(t => t.Name == "TestClass");
+			Assert.IsNotNull(testClass);
+		}
 
-        [Test]
-        public void Should_find_class_with_method_decorator()
-        {
-            var types = _moduleWeaver.GetTypesWithDecoratorAttribute();
+		[Test]
+		public void Should_find_class_with_method_decorator()
+		{
+			var types = _moduleWeaver.GetTypesWithDecoratorAttribute();
 
-            Assert.IsNotNull(types);
-            Assert.AreEqual(types[0].Name, "TestClass");
-        }
+			Assert.IsNotNull(types);
+			Assert.AreEqual(types[0].Name, "TestClass");
+		}
 
-        [Test]
-        public void Should_find_methods_with_method_decorator()
-        {
-            var methods = _moduleWeaver.GetMethodsWithDecoratorAttributeOfType();
+		[Test]
+		public void Should_find_methods_with_method_decorator()
+		{
+			var methods = _moduleWeaver.GetMethodsWithDecoratorAttributeOfType();
 
-            Assert.IsNotNull(methods);
-            Assert.AreEqual(methods[0].Name, "Test");
-        }
-    }
+			Assert.IsNotNull(methods);
+			Assert.AreEqual(methods[0].Name, "Test");
+		}
+	}
 }
