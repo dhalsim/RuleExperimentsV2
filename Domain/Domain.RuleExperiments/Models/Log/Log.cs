f54e122c2dcb1d9d3a6b23b6352b5b9077e44583
dhalsim <barisaydek@Barss-MacBook-Pro.local>
2014-12-26 21:10:43 +0200

    .gitignore is now working

--- Domain/Domain.RuleExperiments/Models/Log/Log.cs
+++ Domain/Domain.RuleExperiments/Models/Log/Log.cs
@@ -2,26 +2,26 @@
 
 namespace Domain.RuleExperiments.Models.Log
 {
-    public class Log
-    {
-        private DateTime Time { get; set; }
+	public class Log
+	{
+		private DateTime Time { get; set; }
 
-        private LogLevel Level { get; set; }
+		public LogLevel Level { get; private set; }
 
-        private string Message { get; set; }
+		private string Message { get; set; }
 
-        public Log(string message)
-        {
-            Message = message;
-            Level = LogLevel.Error;
-            Time = DateTime.Now;
-        }
+		public Log (string message)
+		{
+			Message = message;
+			Level = LogLevel.Error;
+			Time = DateTime.Now;
+		}
 
-        public Log(LogLevel level, string message)
-        {
-            Message = message;
-            Level = level;
-            Time = DateTime.Now;
-        }
-    }
+		public Log (LogLevel level, string message)
+		{
+			Message = message;
+			Level = level;
+			Time = DateTime.Now;
+		}
+	}
 }
--- Tests/UnitTests.RuleExperiments/LoggerTests/StaticLoggerFixture.cs
+++ Tests/UnitTests.RuleExperiments/LoggerTests/StaticLoggerFixture.cs
@@ -4,19 +4,20 @@ using NUnit.Framework;
 
 namespace UnitTests.RuleExperiments.LoggerTests
 {
-    [TestFixture]
-    public class StaticLoggerFixture
-    {
-        [Test]
-        public void Should_log()
-        {
-            StaticLogger logger = new StaticLogger();
-            Log log = new Log(LogLevel.Debug, "log example");
-            logger.Log(log);
+	[TestFixture]
+	public class StaticLoggerFixture
+	{
+		[Test]
+		public void Should_log()
+		{
+			StaticLogger logger = new StaticLogger();
+			Log log = new Log(LogLevel.Debug, "log example");
+			logger.Log(log);
 
-            var logged = logger.GetLogs()[0];
+			var logged = logger.GetLogs()[0];
             
-            Assert.AreSame(log, logged);
-        }
-    }
+			Assert.AreSame(log, logged);
+			Assert.AreEqual(LogLevel.Debug, logged.Level);
+		}
+	}
 }
