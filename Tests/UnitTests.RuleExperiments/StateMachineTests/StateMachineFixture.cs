ad9ba8c4eda41a78063bb6b8cc41060c14903095
dhalsim <baris.aydek@gmail.com>
2014-12-26 10:45:03 +0200

    what is this I don't know

--- Tests/UnitTests.RuleExperiments/StateMachineTests/StateMachineFixture.cs
+++ Tests/UnitTests.RuleExperiments/StateMachineTests/StateMachineFixture.cs
@@ -9,16 +9,19 @@ namespace UnitTests.RuleExperiments.StateMachineTests
 		[Test]
 		public void Should_create_a_state_machine()
 		{
-			IStateMachine stateMachine = SimpleStateMachine.Create();
-			StartState start = new StartState("Start");
-			State search = new State("Search", start);
+            StartState start = new StartState("Start");
+		    State search = new State("search", start);
 
-			stateMachine.AddState(start)
-				.AddState(search);
-
+		    var stateMachine = SimpleStateMachine.Create()
+                .AddStartState(start)
+		        .AddState(search)
+		        .Start()
+		        .NextState("search");
+            
 			Assert.IsNotNull(stateMachine);
-			Assert.IsTrue(start.To.Contains(search));
+            Assert.IsTrue(start.To.Contains(search));
 			Assert.IsTrue(search.From.Contains(start));
+            Assert.AreEqual(stateMachine.CurrentState, search);
 		}
 	}
 }
