962a965f03356629aac52d4561fa08e55d27145a
dhalsim <barisaydek@Barss-MacBook-Pro.local>
2014-12-28 23:19:45 +0200

    delete dlls

---
+++ Implementations/Application.RuleExperiments/Application/Flight/Flight.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using StateMachine;
+
+namespace Application.RuleExperiments.Application.Flight
+{
+	public class Flight
+	{
+		public IStateMachine StateMachine { get; set; }
+
+		public Flight (IStateMachine stateMachine)
+		{
+			StateMachine = stateMachine;
+		}
+
+		public void Search()
+		{
+
+		}
+
+		public void Reset()
+		{
+
+		}
+
+		public void Result()
+		{
+
+		}
+
+		public void Basket()
+		{
+
+		}
+
+		public void Passenger()
+		{
+
+		}
+
+		public void Book()
+		{
+
+		}
+
+		public void Summary()
+		{
+
+		}
+	}
+}
+
---
+++ Tests/UnitTests.RuleExperiments/Application/FlightFixture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using HaveBox;
+using StateMachine;
+using Application.RuleExperiments.Application.Flight;
+using Domain.RuleExperiments;
+using IocContainer.RuleExperiments.HaveBox;
+
+namespace UnitTests.RuleExperiments.Application
+{
+	[TestFixture]
+	public class FlightFixture
+	{
+		IContainer _container;
+		Flight _flight;
+
+		// states
+		StartState _startState;
+		State _searchState;
+
+		[TestFixtureSetUp]
+		public void SetUp()
+		{
+			// instantiate states
+			_startState = new StartState("Started");
+			_searchState = new State("Search", _startState);
+
+			_container = new Container();
+			_container.Configure(cfg => cfg.For<BaseStateMachine>().Use<SimpleStateMachine>().AsSingleton());
+
+			IocContainerFactory.Current = new IocContainerImplementation(_container);
+
+			var stateMachine = _container.GetInstance<IStateMachine>();
+			stateMachine.AddStartState(_startState)
+				.AddState(_searchState);
+
+			_flight = new Flight(IocContainerFactory.Current.GetInstance<IStateMachine>());
+		}
+
+		[Test]
+		public void Should_search_state_machine()
+		{
+			Assert.AreEqual(_flight.StateMachine.CurrentState, _startState);
+			Assert.AreEqual(_flight.StateMachine.History.Count, 0);
+
+			_flight.Search();
+
+			Assert.AreEqual(_flight.StateMachine.CurrentState, _searchState);
+			Assert.AreEqual(_flight.StateMachine.History.Count, 1);
+			Assert.AreEqual(_flight.StateMachine.History[0].State, _startState);
+		}
+	}
+}
+
